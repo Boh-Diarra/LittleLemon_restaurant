6d3912effbccc14fa534b5d1be1fb463126b3dcc
Boh-Diarra <boh.diarra92@gmail.com>
2025-09-02 00:07:24 +0000

    Add restaurant

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
