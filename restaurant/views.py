ac9a3c952f1b87ef8f9734f4205847478b15f38f
Boh-Diarra <boh.diarra92@gmail.com>
2025-09-03 12:47:10 +0000

    Mises en places de l'API menu et Booking pour la reservation de tables

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemVIew(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+
+    
